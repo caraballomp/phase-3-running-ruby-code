5c26335ceeab2b61aa1455138eb6be0985678cd2
Michelle Caraballo <caraballomp@yahoo.com>
2022-04-04 10:03:29 -0700

    commit tree ff148049df9a7b3f7d63dab31900ebe87629f986

---
+++ app.rb
@@ -0,0 +1,21 @@
+# app.rb
+# puts "Hello world!"
+# puts "Hello world!"
+# puts "Hello world!"
+
+# app.rb
+# print "Hello world!"
+
+# print "Hello world!"
+
+# print "Hello world!"
+
+# puts [1, 2, 3, 4, 5]
+# p [1, 2, 3]
+# pp [{ id: 1, hello: "World" }, { id: 2, hello: "Ruby" }, { id: 3, hello: "Moon" }, { id: 4, hello: "Learner" }]
+
+puts "Hello World!"
+
+print "Pass this test, please."
+
+p [1, 2, 3]
